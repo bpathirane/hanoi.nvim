756f0ab4a4df7247f5dfc4ab4cfc96a049cd3fa5
Buddhi Pathirane <buddhi@Mac.lan>
2025-04-06 19:30:33 -0400

    Replacing .vim file with .lua version

--- plugin/hanoi.nvim.lua
+++ plugin/hanoi.nvim.lua
@@ -1,1 +1,5 @@
-print "Hanoi plugin executed ... "
+local M = require("hanoi")
+
+vim.api.nvim_create_user_command("TowersOfHanoi", function()
+  M.open_towers_of_hanoi()
+end, {})
--- plugin/hanoi.vim
+++
@@ -1,2 +0,0 @@
-" Register the TowersOfHanoi command
-command! TowersOfHanoi lua require('hanoi').open_towers_of_hanoi()
